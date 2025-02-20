95d7059d2b8f1f39bd95708b9a43b4e867945354
KierenY <107174786+flq367@users.noreply.github.com>
2025-02-21 00:34:16 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,1 +1,1 @@
-curl -fsSL https://raw.githubusercontent.com/flq367/reality/main/rimenu.sh -o /usr/local/bin/rimenu && chmod +x /usr/local/bin/rimenu && echo "alias r='rimenu'" >> ~/.bashrc && source ~/.bashrc && clear && /usr/local/bin/rimenu
+sudo curl -fsSL https://raw.githubusercontent.com/flq367/reality/main/rimenu.sh -o /usr/local/bin/rimenu && sudo chmod +x /usr/local/bin/rimenu && echo 'alias r="/usr/local/bin/rimenu"' >> ~/.bashrc && source ~/.bashrc && /usr/local/bin/rimenu

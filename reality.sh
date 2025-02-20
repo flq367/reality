259e9a3a5eac853132f53e4c21f2ccc9f0c81d14
KierenY <107174786+flq367@users.noreply.github.com>
2025-02-20 12:17:03 +0800

    Update reality.sh

--- reality.sh
+++ reality.sh
@@ -1,58 +1,8 @@
-[[reality.sh]]
-#!/bin/bash
-export PORT=${PORT:-'8880'}
-export UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
-export SNI=${SNI:-'www.apple.com'}  # 默认伪装网站为 www.apple.com
-
-# 检查是否为root下运行
-[[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && sleep 1 && exit 1
-
-# 安装依赖
-Install_dependencies() {
-    packages="gawk curl openssl"
-    install=""
-
-    for pkg in $packages; do
-        if ! command -v $pkg &>/dev/null; then
-            install="$install $pkg"
-        fi
-    done
-
-    if [ -z "$install" ]; then
-        echo -e "\e[1;32mAll packages are already installed\e[0m"
-        return
-    fi
-
-    if command -v apt &>/dev/null; then
-        pm="apt-get install -y -q"
-    elif command -v dnf &>/dev/null; then
-        pm="dnf install -y"
-    elif command -v yum &>/dev/null; then
-        pm="yum install -y"
-    elif command -v apk &>/dev/null; then
-        pm="apk add"
-    else
-        echo -e "\e[1;33m暂不支持的系统!\e[0m"
-        exit 1
-    fi
-    $pm $install
-}
-Install_dependencies
-
-# 获取IP地址
-getIP() {
-    local serverIP
-    serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
-    if [[ -z "${serverIP}" ]]; then
-        serverIP=$(curl -s --max-time 1 ipv6.ip.sb)
-    fi
-    echo "${serverIP}"
-}
-
 # 安装xray
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+echo "正在安装 Xray..."
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install >/dev/null 2>&1
 
-# 配置Xray
+# 配置Xray并输出结果
 reconfig() {
     reX25519Key=$(/usr/local/bin/xray x25519)
     rePrivateKey=$(echo "${reX25519Key}" | head -1 | awk '{print $3}')
@@ -108,8 +58,8 @@ reconfig() {
 }
 EOF
 
-    # 启动Xray服务
-    systemctl enable xray.service && systemctl restart xray.service
+    systemctl enable xray.service >/dev/null 2>&1
+    systemctl restart xray.service >/dev/null 2>&1
 
     # 获取ipinfo
     ISP=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
@@ -119,10 +69,15 @@ EOF
 
     url="vless://${UUID}@$(getIP):${PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${rePublicKey}&sid=${shortId}&type=tcp&headerType=none#$ISP"
 
+    # 输出精简后的信息
     echo ""
-    echo -e "\e[1;32mreality 安装成功\033[0m"
+    echo "Reality 安装成功！"
     echo ""
-    echo -e "\e[1;32m${url}\033[0m"
+    echo "监听端口: $PORT"
+    echo "伪装网站: $SNI"
+    echo ""
+    echo "链接配置:"
+    echo "$url"
     echo ""
 }
 reconfig
